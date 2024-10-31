a98e84989f9339fe29a71b79c01c0f4864e93093
*(int*)0 = 0xDEADBEEF <arrogancejustified@gmail.com>
2024-10-31 18:33:01 +0300

    [Engine] Finish FILE IO system

--- engine/sys_win.c
+++ engine/sys_win.c
@@ -4,10 +4,16 @@
 #include "winquake.h"
 
 
-void MaskExceptions (void);
-void Sys_InitFloatTime (void);
-void Sys_PushFPCW_SetHigh (void);
-void Sys_PopFPCW (void);
+void MaskExceptions( void );
+void Sys_InitFloatTime( void );
+void Sys_PushFPCW_SetHigh( void );
+void Sys_PopFPCW( void );
+
+
+
+int		(*VID_ForceUnlockedAndReturnState)( void );
+int		(*VID_ForceLockState)( int lk );
+
 
 volatile int					sys_checksum;
 
@@ -59,6 +65,160 @@ int	findhandle( void )
 	Sys_Error("out of handles");
 	return -1;
 }
+
+/*
+================
+filelength
+================
+*/
+int filelength( FILE* f )
+{
+	int		pos;
+	int		end;
+	int		t;
+
+	t = VID_ForceUnlockedAndReturnState();
+
+	pos = ftell(f);
+	fseek(f, 0, SEEK_END);
+	end = ftell(f);
+	fseek(f, pos, SEEK_SET);
+
+	VID_ForceLockState(t);
+
+	return end;
+}
+
+int Sys_FileOpenRead( char* path, int* hndl )
+{
+	FILE* f;
+	int		i, retval;
+	int		t;
+
+	t = VID_ForceUnlockedAndReturnState();
+
+	i = findhandle();
+
+	f = fopen(path, "rb");
+
+	if (!f)
+	{
+		*hndl = -1;
+		retval = -1;
+	}
+	else
+	{
+		sys_handles[i] = f;
+		*hndl = i;
+		retval = filelength(f);
+	}
+
+	VID_ForceLockState(t);
+
+	return retval;
+}
+
+int Sys_FileOpenWrite( char* path )
+{
+	FILE* f;
+	int		i;
+	int		t;
+
+	t = VID_ForceUnlockedAndReturnState();
+
+	i = findhandle();
+
+	f = fopen(path, "wb");
+	if (!f)
+		Sys_Error("Error opening %s: %s", path, strerror(errno));
+	sys_handles[i] = f;
+
+	VID_ForceLockState(t);
+
+	return i;
+}
+
+void Sys_FileClose( int handle )
+{
+	int		t;
+
+	t = VID_ForceUnlockedAndReturnState();
+	fclose(sys_handles[handle]);
+	sys_handles[handle] = NULL;
+	VID_ForceLockState(t);
+}
+
+void Sys_FileSeek( int handle, int position )
+{
+	int		t;
+
+	t = VID_ForceUnlockedAndReturnState();
+	fseek(sys_handles[handle], position, SEEK_SET);
+	VID_ForceLockState(t);
+}
+
+int Sys_FileRead( int handle, void* dest, int count )
+{
+	int		t, x;
+
+	t = VID_ForceUnlockedAndReturnState();
+	x = fread(dest, 1, count, sys_handles[handle]);
+	VID_ForceLockState(t);
+	return x;
+}
+
+int Sys_FileWrite( int handle, void* data, int count )
+{
+	int		t, x;
+	
+	t = VID_ForceUnlockedAndReturnState();
+	x = fwrite(data, 1, count, sys_handles[handle]);
+	VID_ForceLockState(t);
+	return x;
+}
+
+int	Sys_FileTime( char* path )
+{
+	FILE* f;
+	int		t, retval;
+
+	t = VID_ForceUnlockedAndReturnState();
+
+	f = fopen(path, "rb");
+
+	if (f)
+	{
+		fclose(f);
+		retval = 1;
+	}
+	else
+	{
+		retval = -1;
+	}
+
+	VID_ForceLockState(t);
+	return retval;
+}
+
+void Sys_mkdir( char* path )
+{
+	_mkdir(path);
+}
+
+int Sys_FileTell( int i )
+{
+	return ftell(sys_handles[i]);
+}
+
+
+/*
+===============================================================================
+
+SYSTEM IO
+
+===============================================================================
+*/
+
 
 
 
--- engine/winquake.h
+++ engine/winquake.h
@@ -3,3 +3,9 @@
 #pragma warning( disable : 4229 )  // mgraph gets this
 
 #include <windows.h>
+
+
+
+
+int (*VID_ForceUnlockedAndReturnState)( void );
+int (*VID_ForceLockState)( int lk );
