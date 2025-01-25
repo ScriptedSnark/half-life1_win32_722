1a7b71a57edb4323300ea795d0c991dff52dfb4f
*(int*)0 = 0xDEADBEEF <arrogancejustified@gmail.com>
2025-01-25 19:55:08 +0300

    [Engine] Finish GL_WARP.C

--- engine/GL_WARP.C
+++ engine/GL_WARP.C
@@ -733,8 +733,222 @@ retry:
 }
 
 
-// TODO: Implement
+vec3_t	skyclip[6] = {
+	{1,1,0},
+	{1,-1,0},
+	{0,-1,1},
+	{0,1,1},
+	{1,0,1},
+	{-1,0,1}
+};
+int	c_sky;
 
+// 1 = s, 2 = t, 3 = 2048
+int	st_to_vec[6][3] =
+{
+	{3,-1,2},
+	{-3,1,2},
+
+	{1,3,2},
+	{-1,-3,2},
+
+	{-2,-1,3},		// 0 degrees yaw, look straight up
+	{2,-1,-3}		// look straight down
+
+//	{-1,2,3},
+//	{1,2,-3}
+};
+
+// s = [0]/[2], t = [1]/[2]
+int	vec_to_st[6][3] =
+{
+	{-2,3,1},
+	{2,3,-1},
+
+	{1,3,2},
+	{-1,3,-2},
+
+	{-2,-1,3},
+	{-2,1,-3}
+
+//	{-1,2,3},
+//	{1,2,-3}
+};
+
+float	skymins[2][6], skymaxs[2][6];
+
+void DrawSkyPolygon( int nump, vec_t* vecs )
+{
+	int		i, j;
+	vec3_t	v, av;
+	float	s, t, dv;
+	int		axis;
+	float* vp;
+
+	c_sky++;
+#if 0
+	qglBegin(GL_POLYGON);
+	for (i = 0; i < nump; i++, vecs += 3)
+	{
+		VectorAdd(vecs, r_origin, v);
+		qglVertex3fv(v);
+	}
+	qglEnd();
+	return;
+#endif
+	// decide which face it maps to
+	VectorCopy(vec3_origin, v);
+	for (i = 0, vp = vecs; i < nump; i++, vp += 3)
+	{
+		VectorAdd(vp, v, v);
+	}
+	av[0] = fabs(v[0]);
+	av[1] = fabs(v[1]);
+	av[2] = fabs(v[2]);
+	if (av[0] > av[1] && av[0] > av[2])
+	{
+		if (v[0] < 0)
+			axis = 1;
+		else
+			axis = 0;
+	}
+	else if (av[1] > av[2] && av[1] > av[0])
+	{
+		if (v[1] < 0)
+			axis = 3;
+		else
+			axis = 2;
+	}
+	else
+	{
+		if (v[2] < 0)
+			axis = 5;
+		else
+			axis = 4;
+	}
+
+	// project new texture coords
+	for (i = 0; i < nump; i++, vecs += 3)
+	{
+		j = vec_to_st[axis][2];
+		if (j > 0)
+			dv = vecs[j - 1];
+		else
+			dv = -vecs[-j - 1];
+
+		j = vec_to_st[axis][0];
+		if (j < 0)
+			s = -vecs[-j - 1] / dv;
+		else
+			s = vecs[j - 1] / dv;
+		j = vec_to_st[axis][1];
+		if (j < 0)
+			t = -vecs[-j - 1] / dv;
+		else
+			t = vecs[j - 1] / dv;
+
+		if (s < skymins[0][axis])
+			skymins[0][axis] = s;
+		if (t < skymins[1][axis])
+			skymins[1][axis] = t;
+		if (s > skymaxs[0][axis])
+			skymaxs[0][axis] = s;
+		if (t > skymaxs[1][axis])
+			skymaxs[1][axis] = t;
+	}
+}
+
+#define	MAX_CLIP_VERTS	64
+void ClipSkyPolygon( int nump, vec_t* vecs, int stage )
+{
+	float* norm;
+	float* v;
+	qboolean	front, back;
+	float	d, e;
+	float	dists[MAX_CLIP_VERTS];
+	int		sides[MAX_CLIP_VERTS];
+	vec3_t	newv[2][MAX_CLIP_VERTS];
+	int		newc[2];
+	int		i, j;
+
+	if (nump > MAX_CLIP_VERTS - 2)
+		Sys_Error("ClipSkyPolygon: MAX_CLIP_VERTS");
+	if (stage == 6)
+	{	// fully clipped, so draw it
+		DrawSkyPolygon(nump, vecs);
+		return;
+	}
+
+	front = back = FALSE;
+	norm = skyclip[stage];
+	for (i = 0, v = vecs; i < nump; i++, v += 3)
+	{
+		d = DotProduct(v, norm);
+		if (d > ON_EPSILON)
+		{
+			front = TRUE;
+			sides[i] = SIDE_FRONT;
+		}
+		else if (d < ON_EPSILON)
+		{
+			back = TRUE;
+			sides[i] = SIDE_BACK;
+		}
+		else
+			sides[i] = SIDE_ON;
+		dists[i] = d;
+	}
+
+	if (!front || !back)
+	{	// not clipped
+		ClipSkyPolygon(nump, vecs, stage + 1);
+		return;
+	}
+
+	// clip it
+	sides[i] = sides[0];
+	dists[i] = dists[0];
+	VectorCopy(vecs, (vecs + (i * 3)));
+	newc[0] = newc[1] = 0;
+
+	for (i = 0, v = vecs; i < nump; i++, v += 3)
+	{
+		switch (sides[i])
+		{
+			case SIDE_FRONT:
+				VectorCopy(v, newv[0][newc[0]]);
+				newc[0]++;
+				break;
+			case SIDE_BACK:
+				VectorCopy(v, newv[1][newc[1]]);
+				newc[1]++;
+				break;
+			case SIDE_ON:
+				VectorCopy(v, newv[0][newc[0]]);
+				newc[0]++;
+				VectorCopy(v, newv[1][newc[1]]);
+				newc[1]++;
+				break;
+		}
+
+		if (sides[i] == SIDE_ON || sides[i + 1] == SIDE_ON || sides[i + 1] == sides[i])
+			continue;
+
+		d = dists[i] / (dists[i] - dists[i + 1]);
+		for (j = 0; j < 3; j++)
+		{
+			e = v[j] + d * (v[j + 3] - v[j]);
+			newv[0][newc[0]][j] = e;
+			newv[1][newc[1]][j] = e;
+		}
+		newc[0]++;
+		newc[1]++;
+	}
+
+	// continue
+	ClipSkyPolygon(newc[0], newv[0][0], stage + 1);
+	ClipSkyPolygon(newc[1], newv[1][0], stage + 1);
+}
 
 /*
 =================
@@ -743,7 +957,28 @@ R_DrawSkyChain
 */
 void R_DrawSkyChain( msurface_t* s )
 {
-	// TODO: Implement
+	msurface_t* fa;
+
+	int		i;
+	vec3_t	verts[MAX_CLIP_VERTS];
+	glpoly_t* p;
+
+	c_sky = 0;
+
+	// calculate vertex values for sky box
+
+	for (fa = s; fa; fa = fa->texturechain)
+	{
+		for (p = fa->polys; p; p = p->next)
+		{
+			for (i = 0; i < p->numverts; i++)
+			{
+				VectorSubtract(p->verts[i], r_origin, verts[i]);
+			}
+
+			ClipSkyPolygon(p->numverts, verts[0], 0);
+		}
+	}
 
 	R_DrawSkyBox();
 }
@@ -756,23 +991,133 @@ R_ClearSkyBox
 */
 void R_ClearSkyBox( void )
 {
-	// TODO: Implement
+	int		i;
+
+	for (i = 0; i < 6; i++)
+	{
+		skymins[0][i] = skymins[1][i] = 9999;
+		skymaxs[0][i] = skymaxs[1][i] = -9999;
+	}
 }
 
 
-// TODO: Implement
+#define SQRT3INV		(0.57735f)		// a little less than 1 / sqrt(3)
 
+void MakeSkyVec( float s, float t, int axis )
+{
+	vec3_t		v, b;
+	int			j, k;
+	float		width;
 
+	width = gl_zmax.value * SQRT3INV;
+
+	if (s < -1)
+		s = -1;
+	else if (s > 1)
+		s = 1;
+	if (t < -1)
+		t = -1;
+	else if (t > 1)
+		t = 1;
+
+	b[0] = s * width;
+	b[1] = t * width;
+	b[2] = width;
+
+	for (j = 0; j < 3; j++)
+	{
+		k = st_to_vec[axis][j];
+		if (k < 0)
+			v[j] = -b[-k - 1];
+		else
+			v[j] = b[k - 1];
+		v[j] += r_origin[j];
+	}
+
+	// avoid bilerp seam
+	s = (s + 1) * 0.5;
+	t = (t + 1) * 0.5;
+
+	if (s < 1.0 / 512)
+		s = 1.0 / 512;
+	else if (s > 511.0 / 512)
+		s = 511.0 / 512;
+	if (t < 1.0 / 512)
+		t = 1.0 / 512;
+	else if (t > 511.0 / 512)
+		t = 511.0 / 512;
+
+	t = 1.0 - t;
+	qglTexCoord2f(s, t);
+	qglVertex3fv(v);
+}
+
+/*
+=================
+R_DrawSkyBox
+=================
+*/
+int skytexorder[6] = { 0, 2, 1, 3, 4, 5 };
+#define SIGN(d)				((d)<0?-1:1)
+static int	gFakePlaneType[6] = { 1, -1, 2, -2, 3, -3 };
 void R_DrawSkyBox( void )
 {
+	int		i;
+	vec3_t	normal;
+	float	r = 1.0;
+	float	g = 1.0;
+	float	b = 1.0;
+
 	GL_DisableMultitexture();
 
-	// TODO: Implement
+	for (i = 0; i < 6; i++)
+	{
+		if (skymins[0][i] >= skymaxs[0][i] || skymins[1][i] >= skymaxs[1][i])
+			continue;
+
+		VectorCopy(vec3_origin, normal);
+		switch (gFakePlaneType[i])
+		{
+		case 1:
+			normal[0] = 1;
+			break;
+
+		case -1:
+			normal[0] = -1;
+			break;
+
+		case 2:
+			normal[1] = 1;
+			break;
+
+		case -2:
+			normal[1] = -1;
+			break;
+
+		case 3:
+			normal[2] = 1;
+			break;
+
+		case -3:
+			normal[2] = -1;
+			break;
+		}
+		// Hack, try to find backfacing surfaces on the inside of the cube to avoid binding their texture
+		if (DotProduct(vpn, normal) < (-1 + 0.70710678))
+			continue;
+
+		GL_Bind(gSkyTexNumber[skytexorder[i]]);
+
+		qglColor3f(1, 1, 1);
+
+		qglBegin(GL_QUADS);
+		MakeSkyVec(skymins[0][i], skymins[1][i], i);
+		MakeSkyVec(skymins[0][i], skymaxs[1][i], i);
+		MakeSkyVec(skymaxs[0][i], skymaxs[1][i], i);
+		MakeSkyVec(skymaxs[0][i], skymins[1][i], i);
+		qglEnd();
+	}
 }
-
-
-// TODO: Implement
-
 
 //===============================================================
 
@@ -783,5 +1128,5 @@ R_InitSky
 */
 void R_InitSky( void )
 {
-	 // TODO: Implement
+	gLoadSky = TRUE;
 }
