e0f0e9766f914a41cb51832e0a06a2dadd0b68a2
*(int*)0 = 0xDEADBEEF <arrogancejustified@gmail.com>
2025-02-11 17:43:09 +0300

    [Engine] Implement GL version of Mod_LoadStudioModel

--- engine/l_studio.c
+++ engine/l_studio.c
@@ -5,7 +5,7 @@
 
 int giTextureSize;
 
-char loadname[32];
+extern char loadname[32];
 
 //=============================================================================
 
@@ -17,56 +17,56 @@ Mod_LoadStudioModel
 */
 void Mod_LoadStudioModel( model_t* mod, void* buffer )
 {
-	byte*				pin;
-	studiohdr_t*		phdr;
-	int					version;
-	char*				cache;
 	int					i;
-	mstudiotexture_t*	ptexture;
-	char				texname[256];
-	unsigned char*		pPal;
+	byte* pin, * pout;
+	studiohdr_t* phdr;
+	mstudiotexture_t* ptexture;
+	byte* block;
 	int					total;
-	int					size;
+	int					version;
+	int					mark;
 
-	pin = (byte *)buffer;
+	pin = (byte*)buffer;
 
-	phdr = (studiohdr_t *)pin;
+	phdr = (studiohdr_t*)pin;
 
-	total = Hunk_LowMark();
+	mark = Hunk_LowMark();
 
 	version = LittleLong(phdr->version);
 	if (version != STUDIO_VERSION)
 	{
-		memset(phdr, 0, sizeof(studiohdr_t));
-
+		memset(phdr, 0, sizeof(*phdr));
 		strcpy(phdr->name, "bogus");
-
-		phdr->length = sizeof(studiohdr_t);
-		phdr->texturedataindex = sizeof(studiohdr_t);
+		phdr->length = sizeof(*phdr);
+		phdr->texturedataindex = sizeof(*phdr);
 	}
 
-	mod->flags = phdr->flags;
 	mod->type = mod_studio;
+	mod->flags = phdr->flags;
 
-	cache = Hunk_AllocName(phdr->length, loadname);
-	ptexture = (mstudiotexture_t *)(((byte *)phdr) + phdr->textureindex);
-	memcpy(cache, phdr, phdr->length);
+	block = (byte*)Hunk_AllocName(phdr->length, loadname);
+	memcpy(block, phdr, phdr->length);
 
-	if (phdr->numtextures > 0)
+	char name[256];
+	byte* pal;
+
+	ptexture = (mstudiotexture_t*)((byte*)block + phdr->textureindex);
+	for (i = 0; i < phdr->numtextures; i++, ptexture++)
 	{
-		for (i = 0; i < phdr->numtextures; i++)
-		{
-			pPal = cache + ptexture->index + ptexture->width + ptexture->height;
-			strcpy(texname, mod->name);
-			strcat(texname, ptexture->name);
-			ptexture->index = GL_LoadTexture(texname, GLT_STUDIO, ptexture->width, ptexture->height, cache + ptexture->index, FALSE, TEX_TYPE_NONE, pPal);
-		}
+		pal = (byte*)block + ptexture->index + (ptexture->width * ptexture->height);
+		strcpy(name, mod->name);
+		strcat(name, ptexture->name);
+		ptexture->index = GL_LoadTexture(name, GLT_STUDIO, ptexture->width, ptexture->height,
+			(byte*)block + ptexture->index, FALSE, TEX_TYPE_NONE, pal);
 	}
-	size = phdr->texturedataindex - phdr->length - total + Hunk_LowMark();
-	Cache_Alloc(&mod->cache, size, mod->name);
-	if (mod->cache.data)
+
+	total = phdr->texturedataindex - phdr->length - mark + Hunk_LowMark();
+	Cache_Alloc(&mod->cache, total, mod->name);
+
+	pout = (byte*)mod->cache.data;
+	if (pout)
 	{
-		memcpy(mod->cache.data, cache, size);
-		Hunk_FreeToLowMark(total);
+		memcpy(pout, block, total);
+		Hunk_FreeToLowMark(mark);
 	}
 }
