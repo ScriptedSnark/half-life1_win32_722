10b5e38d8b5ec1db55a7f7ee3200846c82cee1ec
*(int*)0 = 0xDEADBEEF <arrogancejustified@gmail.com>
2025-02-16 17:55:30 +0300

    [Engine] Implement SV_HullPointContents and SV_RecursiveHullCheck

--- engine/world.c
+++ engine/world.c
@@ -2,6 +2,11 @@
 
 #include "quakedef.h"
 
+// TODO: Implement
+
+int SV_HullPointContents( hull_t* hull, int num, vec_t* p );
+
+// TODO: Implement
 
 /*
 ===============
@@ -14,6 +19,66 @@ void SV_ClearWorld( void )
 	// TODO: Implement
 }
 
+// TODO: Implement
+
+/*
+===============================================================================
+
+POINT TESTING IN HULLS
+
+===============================================================================
+*/
+
+#if	!id386
+
+/*
+===============
+SV_HullPointContents
+
+===============
+*/
+int SV_HullPointContents( hull_t* hull, int num, const vec_t* p )
+{
+	float		d;
+	dclipnode_t* node;
+	mplane_t* plane;
+
+	while (num >= 0)
+	{
+		if (num < hull->firstclipnode || num > hull->lastclipnode)
+			Sys_Error("SV_HullPointContents: bad node number");
+
+		node = hull->clipnodes + num;
+		plane = hull->planes + node->planenum;
+
+		if (plane->type < 3)
+			d = p[plane->type] - plane->dist;
+		else
+			d = DotProduct(plane->normal, p) - plane->dist;
+		if (d < 0)
+			num = node->children[1];
+		else
+			num = node->children[0];
+	}
+
+	return num;
+}
+
+#endif	// !id386
+
+// TODO: Implement
+
+/*
+===============================================================================
+
+LINE TESTING IN HULLS
+
+===============================================================================
+*/
+
+// 1/32 epsilon to keep floating point happy
+#define	DIST_EPSILON	(0.03125)
+
 /*
 ==================
 SV_RecursiveHullCheck
@@ -22,7 +87,135 @@ SV_RecursiveHullCheck
 */
 qboolean SV_RecursiveHullCheck( hull_t* hull, int num, float p1f, float p2f, vec_t* p1, vec_t* p2, trace_t* trace )
 {
-	// TODO: Implement
+	dclipnode_t	*node;
+	mplane_t	*plane;
+	float		t1, t2;
+	float		frac;
+	int			i;
+	vec3_t		mid;
+	int			side;
+	float		midf;
+
+// check for empty
+	if (num < 0)
+	{
+		if (num != CONTENTS_SOLID)
+		{
+			trace->allsolid = FALSE;
+			if (num == CONTENTS_EMPTY)
+				trace->inopen = TRUE;
+			else if (num != CONTENTS_TRANSLUCENT)
+				trace->inwater = TRUE;
+		}
+		else
+			trace->startsolid = TRUE;
+
+		return TRUE;		// empty
+	}
+
+	if (num < hull->firstclipnode || num > hull->lastclipnode || !hull->planes)
+		Sys_Error("SV_RecursiveHullCheck: bad node number");
+
+//
+// find the point distances
+//
+	node = hull->clipnodes + num;
+	plane = hull->planes + hull->clipnodes[num].planenum;
+
+	if (plane->type < 3)
+	{
+		t1 = p1[plane->type] - plane->dist;
+		t2 = p2[plane->type] - plane->dist;
+	}
+	else
+	{
+		t1 = DotProduct(plane->normal, p1) - plane->dist;
+		t2 = DotProduct(plane->normal, p2) - plane->dist;
+	}
+
+#if 1
+	if (t1 >= 0 && t2 >= 0)
+		return SV_RecursiveHullCheck(hull, node->children[0], p1f, p2f, p1, p2, trace);
+	if (t1 < 0 && t2 < 0)
+		return SV_RecursiveHullCheck(hull, node->children[1], p1f, p2f, p1, p2, trace);
+#else
+	if ((t1 >= DIST_EPSILON && t2 >= DIST_EPSILON) || (t2 > t1 && t1 >= 0))
+		return SV_RecursiveHullCheck(hull, node->children[0], p1f, p2f, p1, p2, trace);
+	if ((t1 <= -DIST_EPSILON && t2 <= -DIST_EPSILON) || (t2 < t1 && t1 <= 0))
+		return SV_RecursiveHullCheck(hull, node->children[1], p1f, p2f, p1, p2, trace);
+#endif
+
+// put the crosspoint DIST_EPSILON pixels on the near side
+	if (t1 < 0)
+		frac = (t1 + DIST_EPSILON) / (t1 - t2);
+	else
+		frac = (t1 - DIST_EPSILON) / (t1 - t2);
+	if (frac < 0)
+		frac = 0;
+	if (frac > 1)
+		frac = 1;
+
+	midf = p1f + (p2f - p1f) * frac;
+	for (i = 0; i < 3; i++)
+		mid[i] = p1[i] + frac * (p2[i] - p1[i]);
+
+	side = (t1 < 0);
+
+	// move up to the node
+	if (!SV_RecursiveHullCheck(hull, node->children[side], p1f, midf, p1, mid, trace))
+		return FALSE;
+
+#ifdef PARANOID
+	if (SV_HullPointContents(sv_hullmodel, mid, node->children[side])
+	== CONTENTS_SOLID)
+	{
+		Con_Printf("mid PointInHullSolid\n");
+		return FALSE;
+	}
+#endif
+
+	// NOTE: this recursion can not be optimized because mid would need to be duplicated on a stack
+	if (SV_HullPointContents(hull, node->children[side ^ 1], mid) != CONTENTS_SOLID)
+	{
+		// go past the node
+		return SV_RecursiveHullCheck(hull, node->children[side ^ 1], midf, p2f, mid, p2, trace);
+	}
+
+	if (trace->allsolid)
+		return FALSE;		// never got out of the solid area
+
+//==================
+// the other side of the node is solid, this is the impact point
+//==================
+	if (!side)
+	{
+		VectorCopy(plane->normal, trace->plane.normal);
+		trace->plane.dist = plane->dist;
+	}
+	else
+	{
+		VectorSubtract(vec3_origin, plane->normal, trace->plane.normal);
+		trace->plane.dist = -plane->dist;
+	}
+
+	while (SV_HullPointContents(hull, hull->firstclipnode, mid)
+			== CONTENTS_SOLID)
+	{ // shouldn't really happen, but does occasionally
+		frac -= 0.1;
+		if (frac < 0)
+		{
+			trace->fraction = midf;
+			VectorCopy(mid, trace->endpos);
+			Con_DPrintf("backup past 0\n");
+			return FALSE;
+		}
+		midf = p1f + (p2f - p1f) * frac;
+		for (i = 0; i < 3; i++)
+			mid[i] = p1[i] + frac * (p2[i] - p1[i]);
+	}
+
+	trace->fraction = midf;
+	VectorCopy(mid, trace->endpos);
 
 	return FALSE;
 }
