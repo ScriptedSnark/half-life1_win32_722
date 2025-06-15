3c7de459f285b9e5ccea1b614ceba80c12e116c8
*(int*)0 = 0xDEADBEEF <arrogancejustified@gmail.com>
2025-06-15 19:51:36 +0700

    [Engine] Finish glide.c

--- engine/glide.c
+++ engine/glide.c
@@ -2,7 +2,7 @@
 #include <windows.h>
 #include "winsani_out.h"
 
-#include "quakedef.h"
+#include <quakedef.h>
 
 /*
 ** basic data types
@@ -73,153 +73,129 @@ typedef FxI32 GrCmpFnc_t;
 typedef struct { int size; void* lfbPtr; FxU32 strideInBytes; GrLfbWriteMode_t writeMode; GrOriginLocation_t origin; } GrLfbInfo_t;
 
 typedef struct GrSstPerfStats_s {
-	FxU32  pixelsIn;              /* # pixels processed (minus buffer clears) */
-	FxU32  chromaFail;            /* # pixels not drawn due to chroma key */
-	FxU32  zFuncFail;             /* # pixels not drawn due to Z comparison */
-	FxU32  aFuncFail;             /* # pixels not drawn due to alpha comparison */
-	FxU32  pixelsOut;             /* # pixels drawn (including buffer clears) */
+    FxU32  pixelsIn;              /* # pixels processed (minus buffer clears) */
+    FxU32  chromaFail;            /* # pixels not drawn due to chroma key */
+    FxU32  zFuncFail;             /* # pixels not drawn due to Z comparison */
+    FxU32  aFuncFail;             /* # pixels not drawn due to alpha comparison */
+    FxU32  pixelsOut;             /* # pixels drawn (including buffer clears) */
 } GrSstPerfStats_t;
 
 /*
 ** linear frame buffer functions
 */
 
-typedef FxBool(*LFBLOCK)(GrLock_t type, GrBuffer_t buffer, GrLfbWriteMode_t writeMode, GrOriginLocation_t origin, FxBool pixelPipeline, GrLfbInfo_t* info);
+typedef FxBool(*LFBLOCK)( GrLock_t type, GrBuffer_t buffer, GrLfbWriteMode_t writeMode, GrOriginLocation_t origin, FxBool pixelPipeline, GrLfbInfo_t* info );
 
-typedef FxBool(*LFBUNLOCK)(GrLock_t type, GrBuffer_t buffer);
-typedef void (*GAMMAFUNC)(float value);
-typedef void (*DEPTHFUNC)(GrCmpFnc_t function);
-typedef void (*PERFSTATS)(GrSstPerfStats_t* pStats);
-typedef void (*PERFSTATSRESET)(void);
-typedef void (*TRISTATS)(FxU32* trisProcessed, FxU32* trisDrawn);
-typedef void (*TRISTATSRESET)(void);
-typedef void (*GRIDLE)(void);
-typedef int (*LFBREAD)(int src_buffer, FxU32 src_x, FxU32 src_y, FxU32 src_width, FxU32 src_height, FxU32 dst_stride, void* dst_data);
+typedef FxBool(*LFBUNLOCK)( GrLock_t type, GrBuffer_t buffer );
+typedef void (*GAMMAFUNC)( float value );
+typedef void (*DEPTHFUNC)( GrCmpFnc_t function );
+typedef void (*PERFSTATS)( GrSstPerfStats_t* pStats );
+typedef void (*PERFSTATSRESET)( void );
+typedef void (*TRISTATS)( FxU32* trisProcessed, FxU32* trisDrawn );
+typedef void (*TRISTATSRESET)( void );
+typedef void (*GRIDLE)( void );
+typedef int (*LFBREAD)( int src_buffer, FxU32 src_x, FxU32 src_y, FxU32 src_width, FxU32 src_height, FxU32 dst_stride, void* dst_data );
 
 
 /*
 ** get glide2x module instance
 */
 static HINSTANCE glideDLLInst = NULL;
-void GlideLoad(void)
+void GlideLoad( void )
 {
-	if (glideDLLInst)
-		return;
-	glideDLLInst = GetModuleHandle("glide2x");
+    if (glideDLLInst)
+        return;
+    glideDLLInst = GetModuleHandle("glide2x");
 }
 
 
 /* glide statistics */
-unsigned int GlideStats(void)
+unsigned int GlideStats( void )
 {
-	PERFSTATS grSstPerfStats;
-	PERFSTATSRESET grSstResetPerfStats;
-	GRIDLE grSstIdle;
-	TRISTATS grTriStats;
-	TRISTATSRESET grResetTriStats;
-	GrSstPerfStats_t stats;
-	FxU32 trisProcessed;
+    PERFSTATS grSstPerfStats;
+    PERFSTATSRESET grSstResetPerfStats;
+    GRIDLE grSstIdle;
+    TRISTATS grTriStats;
+    TRISTATSRESET grResetTriStats;
 
-	GlideLoad();
+    GlideLoad();
 
-	if (!glideDLLInst)
-		return 0;
+    if (!glideDLLInst)
+        return 0;
 
-	grSstPerfStats = (PERFSTATS)GetProcAddress(glideDLLInst, "_grSstPerfStats@4");
-	grSstResetPerfStats = (PERFSTATSRESET)GetProcAddress(glideDLLInst, "_grSstResetPerfStats@0");
-	grSstIdle = (GRIDLE)GetProcAddress(glideDLLInst, "_grSstIdle@0");
-	grTriStats = (TRISTATS)GetProcAddress(glideDLLInst, "_grTriStats@8");
-	grResetTriStats = (TRISTATSRESET)GetProcAddress(glideDLLInst, "_grResetTriStats@0");
+    grSstPerfStats = (PERFSTATS)GetProcAddress(glideDLLInst, "_grSstPerfStats@4");
+    grSstResetPerfStats = (PERFSTATSRESET)GetProcAddress(glideDLLInst, "_grSstResetPerfStats@0");
+    grSstIdle = (GRIDLE)GetProcAddress(glideDLLInst, "_grSstIdle@0");
+    grTriStats = (TRISTATS)GetProcAddress(glideDLLInst, "_grTriStats@8");
+    grResetTriStats = (TRISTATSRESET)GetProcAddress(glideDLLInst, "_grResetTriStats@0");
 
-	if (!grSstPerfStats || !grSstIdle || !grSstResetPerfStats || !grTriStats || !grResetTriStats)
-	{
-		return 0;
-	}
+    if (!grSstPerfStats || !grSstIdle || !grSstResetPerfStats || !grTriStats || !grResetTriStats)
+        return 0;
 
-	grSstIdle();
+    grSstIdle();
 
-	grSstPerfStats(&stats);
+    GrSstPerfStats_t stats;
+    grSstPerfStats(&stats);
 
-	grTriStats(&trisProcessed, NULL);
+    FxU32 trisProcessed;
+    FxU32 trisDrawn;
+    grTriStats(&trisProcessed, &trisDrawn);
 
-	grSstResetPerfStats();
-	grResetTriStats();
+    grSstResetPerfStats();
+    grResetTriStats();
 
-	return stats.pixelsIn;
+    return stats.pixelsIn;
 }
 
 
 /*
 ** read and copy screen pixels to buffer
 */
-int GlideReadPixels(int x, int y, int width, int height, word* pixels)
+int GlideReadPixels( int x, int y, int width, int height, word* pixels )
 {
-	LFBREAD grLfbReadRegion;
-	int     i, j;
-	int     sourcestep, sourcebase;
-	word* pSource, * pDest, * pDestRGB;
-	byte* pSourceBT, * pSourceRGB;
+    LFBREAD grLfbReadRegion;
+    int     row, col;
+    word* topRow, * bottomRow, pixel;
+    byte* rgb;
 
-	GlideLoad();
+    GlideLoad();
 
-	if (!glideDLLInst)
-		return FALSE;
+    if (!glideDLLInst)
+        return FALSE;
 
-	grLfbReadRegion = (LFBREAD)GetProcAddress(glideDLLInst, "_grLfbReadRegion@28");
-	if (!grLfbReadRegion)
-	{
-		return FALSE;
-	}
+    grLfbReadRegion = (LFBREAD)GetProcAddress(glideDLLInst, "_grLfbReadRegion@28");
+    if (!grLfbReadRegion)
+        return FALSE;
 
-	if (!grLfbReadRegion(GR_BUFFER_FRONTBUFFER, x, y, width, height, 2 * width, pixels))
-	{
-		return FALSE;
-	}
+    if (!grLfbReadRegion(GR_BUFFER_FRONTBUFFER, x, y, width, height, 2 * width, pixels))
+        return FALSE;
 
-	pDest = pixels;
-	sourcestep = 0;
-	sourcebase = width * (height - 1);
+    // flip image vertically
+    for (row = 0; row < height / 2; row++)
+    {
+		topRow = pixels + row * width;
+		bottomRow = pixels + (height - 1 - row) * width;
 
-	for (i = 0; i < height / 2; i++)
-	{
-		pSource = &pixels[sourcebase + sourcestep];
-
-		for (j = 0; j < width; j++)
+		for (col = 0; col < width; col++)
 		{
-			GLushort tmp;
-			tmp = pDest[0];
-			pDest[0] = pSource[0];
-			pSource[0] = tmp;
-
-			pDest++;
-			pSource++;
+            word tmp;
+			tmp = topRow[col];
+			topRow[col] = bottomRow[col];
+			bottomRow[col] = tmp;
 		}
+    }
 
-		pDest += width;
-		sourcestep += width;
-		sourcebase += (-2 * width);
-	}
-
-	pSourceBT = (byte*)&pixels[height * width + 1] + height * width;
-	pDest = &pixels[height * width];
-
-	for (i = 0; i < height; i++)
+	// convert from RGB565 to RGB888
+	for (row = 0; row < height; row++)
 	{
-		pDestRGB = pDest;
-		pSourceRGB = pSourceBT;
-
-		for (j = 0; j < width; j++, pSourceRGB -= 3)
+		for (col = 0; col < width; col++)
 		{
-			pDestRGB--;
-
-			// Swap to BGR
-			pSourceRGB[2] = (byte)((UINT)pDestRGB[0] >> 8) & 0xF8;
-			pSourceRGB[1] = (byte)(pDestRGB[0] >> 3) & 0xFC;
-			pSourceRGB[0] = (byte)(pDestRGB[0] << 3) & 0xFF;
+			pixel = pixels[row * width + col];
+			rgb = (byte*)&pixels[3 * (row * width + col)];
+			rgb[0] = (pixel >> 8) & 0xF8;
+			rgb[1] = (pixel >> 3) & 0xFC;
+			rgb[2] = (pixel << 3);
 		}
-
-		pDest = (word*)((byte*)pDest + (-2 * width));
-		pSourceBT += (-3 * width);
 	}
 
 	return TRUE;
