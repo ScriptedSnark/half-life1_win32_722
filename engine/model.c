b56814163c066085aa5f4d31192a54b143792c6a
*(int*)0 = 0xDEADBEEF <arrogancejustified@gmail.com>
2025-08-24 18:58:52 +0700

    [Engine] Finish model.c

--- engine/model.c
+++ engine/model.c
@@ -1718,28 +1718,105 @@ void Mod_LoadAliasModel( model_t* mod, void* buffer )
 
 //=============================================================================
 
+/*
+===============
+Mod_LoadSpriteFrame
+===============
+*/
+void* Mod_LoadSpriteFrame( void* pin, mspriteframe_t** ppframe )
+{
+	dspriteframe_t* pinframe;
+	mspriteframe_t* pspriteframe;
+	int					width, height, size, origin[2];
+
+	pinframe = (dspriteframe_t*)pin;
+
+	width = LittleLong(pinframe->width);
+	height = LittleLong(pinframe->height);
+	size = width * height;
+
+	pspriteframe = (mspriteframe_t*)Hunk_AllocName(sizeof(mspriteframe_t) + size * r_pixbytes,
+		loadname);
+
+	Q_memset(pspriteframe, 0, sizeof(mspriteframe_t) + size);
+	*ppframe = pspriteframe;
+
+	pspriteframe->width = width;
+	pspriteframe->height = height;
+	origin[0] = LittleLong(pinframe->origin[0]);
+	origin[1] = LittleLong(pinframe->origin[1]);
+
+	pspriteframe->up = origin[1];
+	pspriteframe->down = origin[1] - height;
+	pspriteframe->left = origin[0];
+	pspriteframe->right = width + origin[0];
+
+	if (r_pixbytes == 1)
+	{
+		Q_memcpy(&pspriteframe->pixels[0], (byte*)(pinframe + 1), size);
+	}
+	else if (r_pixbytes == 2)
+	{
+	}
+	else
+	{
+		Sys_Error("Mod_LoadSpriteFrame: driver set invalid r_pixbytes: %d\n",
+			r_pixbytes);
+	}
+
+	return (void*)((byte*)pinframe + sizeof(dspriteframe_t) + size);
+}
 
 
+/*
+=================
+Mod_LoadSpriteGroup
+=================
+*/
+void* Mod_LoadSpriteGroup( void* pin, mspriteframe_t** ppframe )
+{
+	dspritegroup_t* pingroup;
+	mspritegroup_t* pspritegroup;
+	int					i, numframes;
+	dspriteinterval_t* pin_intervals;
+	float* poutintervals;
+	void* ptemp;
 
+	pingroup = (dspritegroup_t*)pin;
+	numframes = LittleLong(pingroup->numframes);
 
+	pspritegroup = (mspritegroup_t*)Hunk_AllocName(sizeof(mspritegroup_t) +
+		(numframes - 1) * sizeof(pspritegroup->frames[0]), loadname);
 
+	pspritegroup->numframes = numframes;
 
+	*ppframe = (mspriteframe_t*)pspritegroup;
 
+	pin_intervals = (dspriteinterval_t*)(pingroup + 1);
 
+	poutintervals = (float*)Hunk_AllocName(numframes * sizeof(float), loadname);
 
+	pspritegroup->intervals = poutintervals;
 
+	for (i = 0; i < numframes; i++)
+	{
+		*poutintervals = LittleFloat(pin_intervals->interval);
+		if (*poutintervals <= 0.0)
+			Sys_Error("Mod_LoadSpriteGroup: interval<=0");
 
+		poutintervals++;
+		pin_intervals++;
+	}
 
+	ptemp = (void*)pin_intervals;
 
+	for (i = 0; i < numframes; i++)
+	{
+		ptemp = Mod_LoadSpriteFrame(ptemp, &pspritegroup->frames[i]);
+	}
 
-
-
-
-
-
-
-
-
+	return ptemp;
+}
 
 
 /*
@@ -1749,7 +1826,90 @@ Mod_LoadSpriteModel
 */
 void Mod_LoadSpriteModel( model_t* mod, void* buffer )
 {
-	// TODO: Implement
+	int					i;
+	int					version;
+	dsprite_t* pin;
+	msprite_t* psprite;
+	int					numframes;
+	int					size;
+	int					palsize;
+	dspriteframetype_t* pframetype;
+
+	byte* palsrc;
+	unsigned short* paldest;
+
+	pin = (dsprite_t*)buffer;
+
+	version = LittleLong(pin->version);
+	if (version != SPRITE_VERSION)
+		Sys_Error("%s has wrong version number (%i should be %i)",
+			mod->name, version, SPRITE_VERSION);
+
+	numframes = LittleLong(pin->numframes);
+
+	palsize = *(unsigned short*)(pin + 1);
+	size = sizeof(msprite_t) + (numframes - 1) * sizeof(psprite->frames) + (palsize * 4 * sizeof(unsigned short)) + 2;
+
+	psprite = (msprite_t*)Hunk_AllocName(size, loadname);
+
+	mod->cache.data = psprite;
+
+	psprite->type = LittleLong(pin->type);
+	psprite->texFormat = LittleLong(pin->texFormat);
+	psprite->maxwidth = LittleLong(pin->width);
+	psprite->maxheight = LittleLong(pin->height);
+	psprite->beamlength = LittleFloat(pin->beamlength);
+	mod->synctype = (synctype_t)LittleLong(pin->synctype);
+	psprite->numframes = numframes;
+
+	mod->mins[0] = mod->mins[1] = -psprite->maxwidth / 2;
+	mod->maxs[0] = mod->maxs[1] = psprite->maxwidth / 2;
+	mod->mins[2] = -psprite->maxheight / 2;
+	mod->maxs[2] = psprite->maxheight / 2;
+
+	psprite->paloffset = sizeof(msprite_t) + (numframes - 1) * sizeof(psprite->frames) + 2;
+
+	palsrc = (byte*)(pin + 1) + 2;
+	paldest = (word*)((byte*)(psprite + 1) + sizeof(psprite->frames) * (numframes - 1));
+	*paldest = palsize;
+
+	for (i = 0; i < palsize; i++, paldest += 4, palsrc += 3)
+	{
+		paldest[0] = palsrc[0];
+		paldest[1] = palsrc[1];
+		paldest[2] = palsrc[2];
+		paldest[3] = 0;
+	}
+
+//
+// load the frames
+//
+	if (numframes < 1)
+		Sys_Error("Mod_LoadSpriteModel: Invalid # of frames: %d\n", numframes);
+
+	mod->numframes = numframes;
+	mod->flags = 0;
+
+	pframetype = (dspriteframetype_t*)((byte*)(pin + 1) + 2 + palsize * 3);
+
+	for (i = 0; i < numframes; i++)
+	{
+		spriteframetype_t	frametype;
+
+		frametype = (spriteframetype_t)LittleLong(pframetype->type);
+		psprite->frames[i].type = frametype;
+
+		if (frametype == SPR_SINGLE)
+		{
+			pframetype = (dspriteframetype_t*)Mod_LoadSpriteFrame(pframetype + 1, &psprite->frames[i].frameptr);
+		}
+		else
+		{
+			pframetype = (dspriteframetype_t*)Mod_LoadSpriteGroup(pframetype + 1, &psprite->frames[i].frameptr);
+		}
+	}
+
+	mod->type = mod_sprite;
 }
 
 //=============================================================================
@@ -1761,5 +1921,17 @@ Mod_Print
 */
 void Mod_Print( void )
 {
-	// TODO: Implement
+	int		i;
+	model_t* mod;
+
+	Con_Printf("Cached models:\n");
+	for (i = 0, mod = mod_known; i < mod_numknown; i++, mod++)
+	{
+		Con_Printf("%8p : %s", mod->cache.data, mod->name);
+		if (mod->needload & NL_UNREFERENCED)
+			Con_Printf(" (!R)");
+		if (mod->needload & NL_NEEDS_LOADED)
+			Con_Printf(" (!P)");
+		Con_Printf("\n");
+	}
 }
