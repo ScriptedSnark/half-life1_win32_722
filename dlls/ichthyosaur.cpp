486f4809a74f8692d189937374cbfeabad5008d9
ScriptedSnark <51358194+ScriptedSnark@users.noreply.github.com>
2025-10-11 15:29:50 +0500

    [hl.dll] Do some nailing again

--- dlls/ichthyosaur.cpp
+++ dlls/ichthyosaur.cpp
@@ -74,7 +74,7 @@ public:
 	void  Swim( void );
 	Vector DoProbe(Vector &Probe);
 
-	void  Vector2String(const Vector* vec, char* str);
+	void  Vector2String( const Vector &vec, char *str );
 	float VectorToPitch( const Vector &vec);
 	float FlPitchDiff( void );
 	float ChangePitch( int speed );
@@ -1092,15 +1092,15 @@ Vector CIchthyosaur::DoProbe(Vector &Probe)
 	return Vector(RANDOM_FLOAT(0,0) * m_flightSpeed, RANDOM_FLOAT(0,0) * m_flightSpeed, RANDOM_FLOAT(0,0) * m_flightSpeed); // ScripedSnark: WTF?
 }
 
-void CIchthyosaur::Vector2String( const Vector *vec, char *str )
+void CIchthyosaur::Vector2String( const Vector &vec, char *str )
 {
 	char strx[30];
 	char stry[30];
 	char strz[30];
 
-	_gcvt(vec->x, 20, strx);
-	_gcvt(vec->y, 20, stry);
-	_gcvt(vec->z, 20, strz);
+	_gcvt(vec.x, 20, strx);
+	_gcvt(vec.y, 20, stry);
+	_gcvt(vec.z, 20, strz);
 
 	strcpy(str, "( ");
 	strcat(str, strx);
