dd6c1d6457c68ee8c3203d5771ae50f68b327413
ScriptedSnark <51358194+ScriptedSnark@users.noreply.github.com>
2025-10-12 20:54:15 +0500

    [hl.dll] Finish effects.cpp

--- dlls/effects.cpp
+++ dlls/effects.cpp
@@ -493,7 +493,6 @@ void CLightning::Spawn( void )
 void CLightning::Precache( void )
 {
 	m_spriteTexture = PRECACHE_MODEL( (char *)STRING(m_iszSpriteName) );
-	CBeam::Precache();
 }
 
 
@@ -1695,124 +1694,6 @@ void CTestEffect::Use( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE u
 	m_flStartTime = gpGlobals->time;
 }
 
-
-
-// Blood effects
-class CBlood : public CPointEntity
-{
-public:
-	void	Spawn( void );
-	void	Use( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value );
-	void	KeyValue( KeyValueData *pkvd );
-
-	inline	int		Color( void ) { return pev->impulse; }
-	inline	float 	BloodAmount( void ) { return pev->dmg; }
-
-	inline	void SetColor( int color ) { pev->impulse = color; }
-	inline	void SetBloodAmount( float amount ) { pev->dmg = amount; }
-	
-	Vector	Direction( void );
-	Vector	BloodPosition( CBaseEntity *pActivator );
-
-private:
-};
-
-LINK_ENTITY_TO_CLASS( env_blood, CBlood );
-
-
-
-#define SF_BLOOD_RANDOM		0x0001
-#define SF_BLOOD_STREAM		0x0002
-#define SF_BLOOD_PLAYER		0x0004
-#define SF_BLOOD_DECAL		0x0008
-
-void CBlood::Spawn( void )
-{
-	pev->solid			= SOLID_NOT;
-	pev->movetype		= MOVETYPE_NONE;
-	pev->effects		= 0;
-	pev->frame			= 0;
-	SetMovedir( pev );
-}
-
-
-void CBlood::KeyValue( KeyValueData *pkvd )
-{
-	if (FStrEq(pkvd->szKeyName, "color"))
-	{
-		int color = atoi(pkvd->szValue);
-		switch( color )
-		{
-		case 1:
-			SetColor( BLOOD_COLOR_YELLOW );
-			break;
-		default:
-			SetColor( BLOOD_COLOR_RED );
-			break;
-		}
-
-		pkvd->fHandled = TRUE;
-	}
-	else if (FStrEq(pkvd->szKeyName, "amount"))
-	{
-		SetBloodAmount( atof(pkvd->szValue) );
-		pkvd->fHandled = TRUE;
-	}
-	else
-		CPointEntity::KeyValue( pkvd );
-}
-
-
-Vector CBlood::Direction( void )
-{
-	if ( pev->spawnflags & SF_BLOOD_RANDOM )
-		return UTIL_RandomBloodVector();
-	
-	return pev->movedir;
-}
-
-
-Vector CBlood::BloodPosition( CBaseEntity *pActivator )
-{
-	if ( pev->spawnflags & SF_BLOOD_PLAYER )
-	{
-		edict_t *pPlayer;
-
-		if ( pActivator && pActivator->pev->flags & FL_CLIENT )
-		{
-			pPlayer = pActivator->edict();
-		}
-		else
-			pPlayer = g_engfuncs.pfnPEntityOfEntIndex( 1 );
-		if ( pPlayer )
-			return (pPlayer->v.origin + pPlayer->v.view_ofs) + Vector( RANDOM_FLOAT(-10,10), RANDOM_FLOAT(-10,10), RANDOM_FLOAT(-10,10) );
-	}
-
-	return pev->origin;
-}
-
-
-void CBlood::Use( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value )
-{
-	if ( pev->spawnflags & SF_BLOOD_STREAM )
-		UTIL_BloodStream( BloodPosition(pActivator), Direction(), Color(), BloodAmount() );
-	else
-		UTIL_BloodDrips( BloodPosition(pActivator), Direction(), Color(), BloodAmount() );
-
-	if ( pev->spawnflags & SF_BLOOD_DECAL )
-	{
-		Vector forward = Direction();
-		Vector start = BloodPosition( pActivator );
-		TraceResult tr;
-
-		UTIL_TraceLine( start, start + forward * BloodAmount() * 2, ignore_monsters, NULL, &tr );
-		if ( tr.flFraction != 1.0 )
-			UTIL_BloodDecalTrace( &tr, Color() );
-	}
-}
-
-
-
 // Screen shake
 class CShake : public CPointEntity
 {
@@ -1951,17 +1832,7 @@ void CFade::Use( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType
 	if ( pev->spawnflags & SF_FADE_MODULATE )
 		fadeFlags |= FFADE_MODULATE;
 
-	if ( pev->spawnflags & SF_FADE_ONLYONE )
-	{
-		if ( pActivator->pev->flags & FL_CLIENT )
-		{
-			UTIL_ScreenFade( pev->rendercolor, Duration(), HoldTime(), pev->renderamt, fadeFlags );
-		}
-	}
-	else
-	{
-		UTIL_ScreenFade( pev->rendercolor, Duration(), HoldTime(), pev->renderamt, fadeFlags );
-	}
+	UTIL_ScreenFade( pev->rendercolor, Duration(), HoldTime(), pev->renderamt, fadeFlags );
 	SUB_UseTargets( this, USE_TOGGLE, 0 );
 }
 
